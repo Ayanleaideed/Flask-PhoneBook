26fa5211730f5adf12075b1277f9d7ff23bf9369
Ayanleaideed <Ayanleaideed@outlook.com>
2023-04-08 21:43:17 -0500

    Second commit

--- PhoneBook/venv/app.py
+++ PhoneBook/venv/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import pandas as pd 
 
 app = Flask(__name__)
 
@@ -10,20 +11,57 @@ def index():
 def add_entry():
     if request.method == 'POST':
         if 'add_btn' in request.form:
-            # Handle form submission for adding an entry
-            # Code to add entry to database or perform other necessary actions
-            add_btn_value = request.form['add_btn']
-            print(f"Add button value: {add_btn_value}")
+            name = request.form.get('name')
+            phone = request.form.get('phone')
+            if name and phone is not None:
+                write_file(name, phone)
             return render_template('add_entry.html')
-        elif 'search' in request.form:
-            # Handle form submission for searching an entry
-            # Code to search database or perform other necessary actions
-            search_query = request.form['search']
-            print(f"Search query: {search_query}")
-            return render_template('index.html')
-    # Handle GET request or other button clicks
     return render_template('index.html')
 
-def add_
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def Search_entry():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        phone = request.form.get('phone')
+        if name and phone:
+            return "Please search by either name or phone number, not both."
+        data = read_file(name, phone)
+        if data:
+            table_rows = []
+            for entry in data:
+                table_rows.append({'name': entry['Name'], 'phone': entry['Phone']})
+            return render_template('results.html', rows=table_rows)
+        else:
+            return "No entries found."
+    return render_template('search.html')
+
+
+
+
+def write_file(name, phone):
+    with open('phone_book.txt', "a+") as file:
+        file.write(f"{str(name)},{(phone)}\n")
+
+
+def read_file(name=None, phone=None):
+    entries = []
+    with open('phone_book.txt', 'r') as file:
+        for line in file:
+            parts = line.strip().split(',')
+            if len(parts) == 2:
+                n, num = parts[0], parts[1]
+                if name and name.lower() not in n.lower():
+                    continue
+                if phone and phone != num.replace('-', ''):
+                    continue
+                entries.append({'Name': n, 'Phone': num})
+    return entries
+
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
